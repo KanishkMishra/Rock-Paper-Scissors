33a1d42ee6a6833a8165c61ccb36d70397d23b1b
Kanishk Mishra <Kanishkmishra3528@gmail.com>
2022-05-25 15:45:54 -0400

    Completed single rounds of play with playRound()

--- rps.js
+++ rps.js
@@ -16,3 +16,27 @@ function computerPlay() {
     }
     return result;
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.slice(0, 1).toUpperCase() + playerSelection.slice(1).toLowerCase();
+    let message;
+
+    if (playerSelection === computerSelection) {
+        message = "Tie Game! You both picked "+computerSelection;
+    } else if (((playerSelection=="Rock")&&(computerSelection=="Scissors")) || ((playerSelection=="Paper")&&(computerSelection=="Rock")) || ((playerSelection=="Scissors")&&(computerSelection=="Paper"))) {
+        message = "You Win! "+playerSelection+" beats "+computerSelection;
+    } else {
+        message ="You Lose! "+computerSelection+" beats "+playerSelection;
+    }
+
+    return message;
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = window.prompt("Rock, Paper or Scissors?");
+        let computerSelection = computerPlay();
+
+        console.log(playerSelection, computerSelection);
+    }
+}
