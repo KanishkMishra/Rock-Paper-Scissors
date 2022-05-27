5d10c60842d3ef9e9312b9c449beac642eb76dbd
Kanishk Mishra <Kanishkmishra3528@gmail.com>
2022-05-27 16:07:08 -0400

    Created UI where buttosn are used and score is kept up to 5

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Rock-Paper-Scissors
 
 This is a computer simulated rock appaer scissors game where the use faces the computer(javascript) and determines a winner.
+
+Upadtes with a user interface that allows play using buttons
--- rps.js
+++ rps.js
@@ -17,7 +17,7 @@ function computerPlay() {
     return result;
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection=computerPlay()) {
     playerSelection = playerSelection.slice(0, 1).toUpperCase() + playerSelection.slice(1).toLowerCase();
     let message;
 
@@ -32,7 +32,8 @@ function playRound(playerSelection, computerSelection) {
     return message;
 }
 
-function game() {
+/*function game() {
+    // play 5 games using typed words
     for (let i = 0; i < 5; i++) {
         const playerSelection = window.prompt("Rock, Paper, Scissors!");
         const computerSelection = computerPlay();
@@ -43,6 +44,104 @@ function game() {
             console.log("Invalid input, Rocks, Papers and Scissors only!!!");
         }
     }
+}*/
+
+// player scores
+let pScore = 0;
+let cScore = 0;
+
+function updateScore(result) {
+    let array = result.split("!");
+    if (array[0] === "You Win") {
+        pScore++; 
+    }
+    else if (array[0] === "You Lose") {
+       cScore++;
+    }
+
+    player.textContent = "Player: "+pScore;
+    computer.textContent = "Computer: "+cScore;
+
+    checkWinner(pScore, cScore);
 }
 
-game()
+function checkWinner(pScore, cScore) {
+    if (pScore==5) {
+        const end = document.querySelector('div');
+
+        end.style.textAlign='center';
+        end.style.justifySelf='center';
+        end.style.fontSize="64px";
+
+        end.textContent="You have won! Congratulations!";
+    } else if (cScore==5) {
+        const end = document.querySelector('div');
+
+        end.style.textAlign='center';
+        end.style.justifySelf='center';
+        end.style.fontSize="64px";
+
+        end.textContent="Computer has won! Try again!";
+    }
+}
+
+// locate buttons
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+
+//click buttons
+rock.addEventListener('click', () => {
+    const board = document.querySelector("h2");
+
+    // give point to winner
+    let result = playRound("Rock");
+    updateScore(result);
+
+    board.textContent = result;
+});
+paper.addEventListener('click', () => {
+    const board = document.querySelector("h2");
+
+    // give point to winner
+    let result = playRound("Paper");
+    updateScore(result);
+
+    board.textContent = playRound("Paper")
+});
+scissors.addEventListener('click', () => {
+    const board = document.querySelector("h2");
+
+    // give point to winner
+    let result = playRound("Scissors");
+    updateScore(result);
+
+    board.textContent = playRound("Scissors")
+});
+
+// create display
+const container = document.querySelector('body');
+
+const display = document.createElement('div');
+
+display.style.border = "thick solid black";
+display.style.height = "400px";
+display.style.margin = "40px";
+display.style.backgroundColor = "blue";
+
+// create score board
+const player = document.createElement('h1');
+const computer = document.createElement('h1');
+const winner = document.createElement('h2');
+
+winner.style.textAlign='center';
+
+updateScore("");
+
+display.appendChild(player);
+display.appendChild(computer);
+display.appendChild(winner);
+
+container.appendChild(display);
+
+//game()
